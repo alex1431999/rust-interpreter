d00ae0c7357b6d6a0f25b059108bddecc3aaf375
alex1431999 <alexhaller99@yahoo.de>
2026-02-17 21:02:15 +0100

    feat: implement basic parser and evaluator

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,3 +1,58 @@
+#[derive(Debug)]
+enum Expr {
+    Number(i64),
+    Binary {
+        left: Box<Expr>,
+        op: Op,
+        right: Box<Expr>,
+    }
+}
+
+#[derive(Debug)]
+enum Op {
+    Add
+}
+
 pub fn execute(code_to_execute: String) {
-    println!("{}", code_to_execute)
+    let tokens: Vec<&str> = code_to_execute.split(" ").collect();
+
+    let ast = parse(tokens);
+
+    let result = eval(&ast);
+    println!("{}", result);
+}
+
+fn parse(tokens: Vec<&str>) -> Expr {
+    if tokens.len() != 3 {
+        panic!("Tokens need to be of length 3")
+    }
+
+    let first_number = tokens[0].parse().expect("First number must be a number");
+    let operation = tokens[1];
+    let second_number = tokens[2].parse().expect("Second number must be a number");
+
+    let operation_expression = match operation {
+        "+" => Op::Add,
+        _ => panic!("Unknown operation {}", operation)
+    };
+
+    Expr::Binary {
+        left: Box::new(Expr::Number(first_number)),
+        op: operation_expression,
+        right: Box::new(Expr::Number(second_number))
+    }
+}
+
+fn eval(expr: &Expr) -> i64 {
+    match expr {
+        Expr::Number(n) => *n,
+        Expr::Binary { left, op, right } => {
+            let left_evaluated = eval(left);
+            let right_evaluated = eval(right);
+
+            match op {
+                Op::Add => left_evaluated + right_evaluated
+            }
+        }
+    }
 }
