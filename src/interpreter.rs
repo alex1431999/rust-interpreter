836284f1977eaf4e2c56daafaa7d82bdaca9e2f2
alex1431999 <alexhaller99@yahoo.de>
2026-02-19 09:56:56 +0100

    feat: tokenize

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,57 +3,96 @@ enum Expr {
     Number(i64),
     Binary {
         left: Box<Expr>,
-        op: Op,
+        operation: Operation,
         right: Box<Expr>,
     }
 }
 
-#[derive(Debug)]
-enum Op {
+#[derive(Debug, Clone)]
+enum Operation {
     Add
 }
 
+#[derive(Debug, Clone)]
+enum Token {
+    Number(i64),
+    Operation(Operation)
+}
+
 pub fn execute(code_to_execute: &str) -> i64 {
-    let tokens: Vec<&str> = code_to_execute.split(" ").collect();
+    let tokens = tokenize(code_to_execute);
 
     let ast = parse(tokens);
 
    eval(&ast)
 }
 
+fn tokenize(code_to_execute: &str) -> Vec<Token> {
+    let words: Vec<&str> = code_to_execute.split(" ").collect();
+    words.iter().map(|word| {
+        if *word == "+" {
+            return Token::Operation(Operation::Add)
+        }
+
+        if word.parse::<i64>().is_ok() {
+            return Token::Number(word.parse().unwrap())
+        }
+
+        panic!("invalid syntax {}", word)
+    }).collect()
+
+}
+
 // TODO the parser is super simple and hardcoded. It expects exactly 3 tokens which of course
 //  doesn't always have to be the case. Next we should allow for "infinite" additions by making
 //  the parser recursive.
-fn parse(tokens: Vec<&str>) -> Expr {
+fn parse(tokens: Vec<Token>) -> Expr {
     if tokens.len() != 3 {
-        panic!("Tokens need to be of length 3")
+        panic!("We expect exactly 3 tokens")
     }
 
-    let first_number = tokens[0].parse().expect("First number must be a number");
-    let operation = tokens[1];
-    let second_number = tokens[2].parse().expect("Second number must be a number");
+    //if tokens.len() == 0 {
+    //    return expr
+    //}
 
-    let operation_expression = match operation {
-        "+" => Op::Add,
-        _ => panic!("Unknown operation {}", operation)
+    //let first_token = tokens[0];
+    
+    // Check if first token is a number
+    // Check if second token is an operation
+    // Check if third token is a number again
+
+    let left = match &tokens[0] {
+        Token::Number(n) => Expr::Number(*n),
+        _ => panic!("First token should be a number")
     };
 
+    let operation = match &tokens[1] {
+        Token::Operation(operation) => operation.clone(),
+        _ => panic!("Second token should be an operation")
+    };
+
+    let right = match &tokens[2] {
+        Token::Number(n) => Expr::Number(*n),
+        _ => panic!("Third token should be a number")
+    };
+
+
     Expr::Binary {
-        left: Box::new(Expr::Number(first_number)),
-        op: operation_expression,
-        right: Box::new(Expr::Number(second_number))
+        left: Box::new(left),
+        operation,
+        right: Box::new(right)
     }
 }
 
 fn eval(expr: &Expr) -> i64 {
     match expr {
         Expr::Number(n) => *n,
-        Expr::Binary { left, op, right } => {
+        Expr::Binary { left, operation: op, right } => {
             let left_evaluated = eval(left);
             let right_evaluated = eval(right);
 
             match op {
-                Op::Add => left_evaluated + right_evaluated
+                Operation::Add => left_evaluated + right_evaluated
             }
         }
     }
@@ -66,5 +105,10 @@ mod tests {
     #[test]
     fn basic_addition() {
         assert_eq!(execute("5 + 5"), 10)
+    }
+
+    #[test]
+    fn three_numbers_addition() {
+        assert_eq!(execute("5 + 5 + 5"), 15)
     }
 }
