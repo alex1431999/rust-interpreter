7b89d4fb74b3a32e4114ba239181559ca54d839c
alex1431999 <alexhaller99@yahoo.de>
2026-02-21 14:47:42 +0100

    feat: parse each character one by one

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,18 +1,38 @@
+use crate::enums::Token::Number;
 use crate::enums::{Operation, Token};
 
 pub fn tokenize(code_to_execute: &str) -> Vec<Token> {
-    let words: Vec<&str> = code_to_execute.split_whitespace().collect();
-    words
-        .iter()
-        .map(|word| match word.parse::<i64>() {
-            Ok(n) => Token::Number(n),
-            Err(_) if *word == "+" => Token::Operation(Operation::Add),
-            Err(_) if *word == "-" => Token::Operation(Operation::Subtract),
-            Err(_) if *word == "*" => Token::Operation(Operation::Multiply),
-            Err(_) if *word == "/" => Token::Operation(Operation::Divide),
-            Err(_) => panic!("Invalid syntax {}", word),
-        })
-        .collect()
+    let mut tokens: Vec<Token> = vec![];
+    let characters: Vec<char> = code_to_execute.chars().collect();
+
+    let mut number_being_parsed: i64 = 0;
+
+    for i in 0..characters.len() {
+        let character = characters[i];
+        let has_next_character = (i + 1) < characters.len();
+
+        if character.is_digit(10) {
+            number_being_parsed = number_being_parsed * 10 + character.to_digit(10).unwrap() as i64;
+
+            if !has_next_character || !characters[i + 1].is_digit(10) {
+                tokens.push(Number(number_being_parsed));
+                number_being_parsed = 0;
+            }
+
+            continue;
+        }
+
+        match character {
+            '+' => tokens.push(Token::Operation(Operation::Add)),
+            '-' => tokens.push(Token::Operation(Operation::Subtract)),
+            '*' => tokens.push(Token::Operation(Operation::Multiply)),
+            '/' => tokens.push(Token::Operation(Operation::Divide)),
+            ' ' => {} // We just ignore white space for now
+            _ => panic!("Unsupported character {}", character),
+        }
+    }
+
+    tokens
 }
 
 mod tests {
@@ -26,6 +46,18 @@ mod tests {
                 Token::Number(5),
                 Token::Operation(Operation::Add),
                 Token::Number(5)
+            ]
+        )
+    }
+
+    #[test]
+    fn no_white_space() {
+        assert_eq!(
+            tokenize("55+55"),
+            vec![
+                Token::Number(55),
+                Token::Operation(Operation::Add),
+                Token::Number(55)
             ]
         )
     }
